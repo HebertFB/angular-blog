c315b35999ef06f104aecbd82137eb7da74f4cb7
hebert farias <hebertfbarbosa@outlook.com>
2023-10-06 16:27:49 -0300

    Angular blog project completed - added dynamism to components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  result: {
+    id: string,
+    photoCover: string,
+    contentTitle: string,
+    contentDescription: string
+  }[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.setValuesToComponent()
   }
 
+  setValuesToComponent() {
+    // Adicionando dados do dataFake no array
+    dataFake.forEach((item) => {
+      this.result.push({
+        id: item?.id,
+        photoCover: item.photo,
+        contentTitle: item.title,
+        contentDescription: item.description,
+      });
+    });
+  }
 }
